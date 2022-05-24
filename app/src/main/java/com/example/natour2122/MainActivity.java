c67c553d7a5b75fa1d57aae7e1e615a33c4250ec
Andrea <andreamigliorisi99@gmail.com>
2022-05-24 22:54:56 +0200

    Inizializzazione app Natour - aggiunta di file di configurazione, schemata iniziale, schermata di login e schermata di registrazione account. Aggiunte le icone di facebook e google come imageButton

---
+++ app/src/main/java/com/example/natour2122/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.natour2122;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+   // private final String TAG = "MainActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn_signin = (Button) findViewById(R.id.buttonSignIn);
+        btn_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               openLoginActivity();
+                //System.out.println("hai cliccato");
+            }
+        });
+
+         Button btn_signup = (Button) findViewById(R.id.buttonSignUp);
+         btn_signup.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 openSignUpActivity();
+             }
+         });
+    }
+    public void openLoginActivity() {
+        Intent intent= new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+     public void openSignUpActivity(){
+         Intent intent_signup=new Intent(this,SignUpActivity.class);
+         startActivity(intent_signup);
+     }
+}
